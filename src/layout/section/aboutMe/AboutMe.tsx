570326eccae6b285e9126cc3037fffb49de7bee1
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-04-12 12:46:25 -0700

    About me

--- src/layout/section/aboutMe/AboutMe.tsx
+++ src/layout/section/aboutMe/AboutMe.tsx
@@ -1,64 +1,73 @@
 import styled from "styled-components";
+import myPhoto from "../../../assets/images/myPhoto.webp"
 
 export const AboutMe = () => {
     return (
         <StyledAboutMe>
-            <Description>
                 <LeftColumn>
-                    <Title>About Me</Title>
-                    <CreativePerspective>I look at usual things with my unsual eyes.</CreativePerspective>
+                    <Text>About Me</Text>
+                    <LeftDescription>I look at usual things with my unsual eyes.</LeftDescription>
+                    <MyPhotoLeft src={myPhoto} alt="myPhoto"/>
                 </LeftColumn>
                 <RightColumn>
-                    <ApproachDescription>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus massa
-                        sollicitudin
-                        amet augue. Nibh metus a semper purus mauris duis. Lorem eu neque, tristique quis duis. Nibh
-                        scelerisque ac adipiscing velit non nulla in amet pellentesque.
-
-                        Sit turpis pretium eget maecenas. Vestibulum dolor mattis consectetur eget commodo vitae. Amet
-                        pellentesque sit pulvinar lorem mi a, euismod risus rhoncus. Elementum ullamcorper nec,
-                        habitasse
-                        vulputate. Eget dictum quis est sed egestas tellus, a lectus. Quam ullamcorper in fringilla arcu
-                        aliquet fames arcu.Lacinia eget faucibus urna, nam risus nec elementum cras porta.
-
-                        Sed elementum, sed dolor purus dolor dui. Ut dictum nulla pulvinar vulputate sit sagittis in
-                        eleifend dignissim. Natoque mauris cras molestie velit. Maecenas eget adipiscing quisque viverra
-                        lectus arcu, tincidunt ultrices pellentesque.
-                    </ApproachDescription>
+                    <RightDescription>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam consectetur
+                        deserunt, dicta
+                        dolor dolorum ea explicabo facilis fugit in ipsa iure libero maxime molestias mollitia optio
+                        pariatur porro quaerat quibusdam quidem quisquam quo quod sed soluta sunt ullam, ut veritatis!
+                        Accusantium animi assumenda, at laboriosam praesentium rem sequi sit voluptatibus! Asperiores
+                        consequatur culpa enim libero quaerat quasi sed voluptatibus. Culpa esse hic laboriosam
+                        perspiciatis? Accusamus aliquid, assumenda at autem beatae dolore ducimus ea earum enim eos esse ex
+                        inventore ipsum iusto laboriosam libero natus necessitatibus non quae rerum sequi sunt tenetur
+                        voluptas. A ab accusamus assumenda beatae consequatur, culpa cumque cupiditate dicta distinctio
+                        dolores eius esse excepturi exercitationem facilis harum libero mollitia nam nisi non nostrum nulla,
+                        possimus quae quaerat quam quasi quis sequi, suscipit? Impedit minus modi quis sapiente soluta
+                        vitae! Aperiam atque consequatur dignissimos ea earum explicabo id illo iure molestiae nisi
+                        obcaecati, placeat praesentium qui quis quisquam quod reiciendis sint suscipit tenetur voluptatum?
+                    </RightDescription>
+                    <MyPhotoRight src={myPhoto} alt="myPhoto"/>
                 </RightColumn>
-
-            </Description>
         </StyledAboutMe>
     );
 };
+
+
 const StyledAboutMe = styled.section`
-    min-height: 100vh;
+    min-width: 1440px;
+    min-height: 1470px;
     background-color: darkcyan;
     display: flex;
-    justify-content: center;
-    align-items: center;
+    justify-content: space-evenly;
+    align-items: stretch;
+    padding-top: 109px;
 `
 const LeftColumn = styled.div`
-    flex: 1;
-    font-size: 48px;
-    font-weight: bold;
-    line-height: 1.2;
-`;
-
+    width: 511px;
+    height: 214px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
 const RightColumn = styled.div`
-    flex: 1;
-    font-size: 16px;
-    line-height: 1.6;
-`;
-
-const Description = styled.div`
-    width: 1312px;
+    width: 437px;
     height: 540px;
     display: flex;
-    gap: 40px;
-    align-items: center; /* Добавлено для вертикального выравнивания */
+    flex-direction: column;
+    align-items: center;
 `
-
-const Title = styled.h2``
-const CreativePerspective = styled.div``;
-const ApproachDescription = styled.div``;
+const Text = styled.span`
+`
+const LeftDescription = styled.div``
+const MyPhotoLeft = styled.img`
+    width: 532px;
+    height: 498px;
+    object-fit: cover;
+`
+const RightDescription = styled.div`
+    width: 437px;
+    height: 540px;
+`
+const MyPhotoRight = styled.img`
+    width: 440px;
+    height: 412px;
+    object-fit: cover;
+`
