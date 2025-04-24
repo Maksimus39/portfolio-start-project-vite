f8c490cce388f60e7d0fc9334966fb3c360024b1
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-04-24 00:33:55 -0700

    footer

--- src/layout/section/footer/Footer.tsx
+++ src/layout/section/footer/Footer.tsx
@@ -8,17 +8,21 @@ export const Footer = () => {
         <StyledFooter>
             <Line/>
             <ContentWrapper>
-                <FlexWrapper direction={"row"} align={"center"}>
-                    <StyledCopyRight>2022 Resume. All right reserved.</StyledCopyRight>
+                <FlexWrapper direction={"row"} align={"center"} justify={"space-evenly"}>
+                    <StyledCopyRight>2025 Resume. All right reserved.</StyledCopyRight>
                     <SocialList>
                         <SocialItem>
                             <SocialLinc>
-                                <Icon height={"18px"} width={"10px"} viewBox={"0 0 18 10"} iconId={"tg"}/>
+                                <a href="https://t.me/@Macsimus_Lipeck" target="_blank" rel="noreferrer">
+                                    <Icon height={"24px"} width={"24px"} viewBox={"0 0 24 24"} iconId={"tg"}/>
+                                </a>
                             </SocialLinc>
                         </SocialItem>
                         <SocialItem>
                             <SocialLinc>
-                                <Icon height={"18px"} width={"10px"} viewBox={"0 0 18 10"} iconId={"vk"}/>
+                                <a href="https://vk.com/al_feed.php" target="_blank" rel="noreferrer">
+                                    <Icon height={"24px"} width={"24px"} viewBox={"0 0 24 24"} iconId={"vk"}/>
+                                </a>
                             </SocialLinc>
                         </SocialItem>
                     </SocialList>
@@ -29,20 +33,19 @@ export const Footer = () => {
 };
 
 const StyledFooter = styled.footer`
-    min-height: 40vh;
+    min-height: 120px;
     background-color: ${Theme.colors.primaryBg};
     width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
 `;
- const Line = styled.hr`
-     margin: 0;
-     width: 1280px;
-     height: 3px;
-     background-color: black;
- `;
-
+const Line = styled.hr`
+    margin: 0;
+    width: 1280px;
+    height: 1px;
+    color: ${Theme.colors.textBlack};
+`;
 const ContentWrapper = styled.div`
     display: flex;
     align-items: center;
@@ -50,18 +53,21 @@ const ContentWrapper = styled.div`
     width: 100%;
     padding-top: 66px;
 `;
-
 const SocialList = styled.ul`
     display: flex;
     gap: 12px;
 `;
-
 const SocialItem = styled.li`
     display: flex;
     list-style: none;
 `;
 const SocialLinc = styled.a``
 
-
 const StyledCopyRight = styled.small`
+    font-family: Roboto, serif;
+    font-weight: 400;
+    font-size: 14px;
+    white-space: nowrap;
+
+
 `;
