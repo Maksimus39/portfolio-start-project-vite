db87d8739158c7899812b485381e1b999bc41fa0
Maksim Minakov <maksim.minakov.83@mail.ru>
2025-06-02 22:33:58 -0700

    add skills

--- src/layout/section/skills/Skills.tsx
+++ src/layout/section/skills/Skills.tsx
@@ -11,18 +11,18 @@ type SkillType = {
 const skill: SkillType[] = [
     {
         index: 0,
-        skillTitle: "React",
-        skillText: "React — это популярная JavaScript-библиотека для создания динамичных и быстрых пользовательских  интерфейсов с помощью компонуемых, переиспользуемых компонентов и виртуального DOM."
-    },
-    {
-        index: 1,
         skillTitle: "HTML",
         skillText: "HTML (HyperText Markup Language) — стандартный язык разметки для создания структуры веб-страниц с помощью тегов, атрибутов и элементов."
     },
     {
-        index: 2,
+        index: 1,
         skillTitle: "CSS",
         skillText: "CSS (Cascading Style Sheets) — язык стилей для оформления HTML-документов, управляющий цветами, шрифтами, макетом и анимацией элементов."
+    },
+    {
+        index: 2,
+        skillTitle: "Styled Components",
+        skillText: "Styled Components — библиотека для CSS-in-JS, позволяющая писать стили в JavaScript с автоматической генерацией уникальных классов и поддержкой темы."
     },
     {
         index: 3,
@@ -31,19 +31,24 @@ const skill: SkillType[] = [
     },
     {
         index: 4,
+        skillTitle: "React",
+        skillText: "React — это популярная JavaScript-библиотека для создания динамичных и быстрых пользовательских интерфейсов с помощью компонуемых, переиспользуемых компонентов и виртуального DOM."
+    },
+    {
+        index: 5,
         skillTitle: "Redux",
         skillText: "Redux — библиотека для управления состоянием JavaScript-приложений через единый хранилище (store), работающая на принципах иммутабельности и одностороннего потока данных."
     },
     {
-        index: 5,
+        index: 6,
         skillTitle: "RTK 2.0",
         skillText: "Redux Toolkit (RTK 2.0) — официальная библиотека для эффективной работы с Redux, упрощающая настройку хранилища, редьюсеров и асинхронной логики."
     },
     {
-        index: 6,
-        skillTitle: "Styled Components",
-        skillText: "Styled Components — библиотека для CSS-in-JS, позволяющая писать стили в JavaScript с автоматической генерацией уникальных классов и поддержкой темы."
-    },
+        index: 7,
+        skillTitle: "Postman",
+        skillText: "Postman — инструмент для тестирования API, автоматизации запросов, мокинга и документирования."
+    }
 ]
 
 export const Skills = () => {
